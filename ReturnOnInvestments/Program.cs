149aeecd9e47037714ae54264e797fb14868e215
PhalanndwaTendani <2023393305@ufs4life.ac.za>
2024-05-23 15:05:17 +0200

    Add files via upload

---
+++ ReturnOnInvestments/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnOnInvestments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Initial amount (R): ");
+            //decimal mAmount = decimal.Parse(Console.ReadLine());
+
+            //Console.Write("Period of investment: ");
+            //int iPeriod = int.Parse(Console.ReadLine());
+
+            //Console.Write("Interest rate per period (%): ");
+            //double dRate = double.Parse(Console.ReadLine()) / 100;
+
+            //mAmount += mAmount * (decimal)dRate;
+
+            //int i = 1;
+
+            //while (i <=iPeriod)
+            //{
+            //    mAmount += mAmount * (decimal)dRate;
+            //    i++;
+            //}
+
+            /*REPEATING NUMBERS/
+             */
+
+            //int i =0;
+
+            //while (i<10)
+            //{
+            //    Console.WriteLine("Number : " + i);
+            //    i++;
+            //}
+
+            Console.Write("N = ");
+            int iN =  int.Parse(Console.ReadLine());
+
+            int iNum = 1;
+            int iSum = 1;
+
+            Console.Write("The sum 1");
+            
+            while (iNum < iN)
+            {
+                iNum++;
+                Console.Write(" + " + iNum);
+                iSum = iSum + iNum;
+            }
+
+            Console.WriteLine(" = " + iSum);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
